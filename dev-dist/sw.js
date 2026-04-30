d5b4d208b7bb3ef2348278b1af7699618e3d82f7
zuiken-code <zuiken2022robo@gmail.com>
2026-05-01 01:41:16 +0900

    delate deploy workflow

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.os6m112fb1s"
+    "revision": "0.6pbuct83a2c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
